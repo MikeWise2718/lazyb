a4c87197a6b8a7523737a85b726aeaf0b1d1d2d6
MikeWise2718 <mikewise1618@gmail.com>
2021-11-16 00:10:44 +0100

    more setup

--- lazyb.py
+++ lazyb.py
@@ -1,4 +1,7 @@
+import argparse
+import time
 
+starttime = time.time()
 
 # build rules
 # HWO harvest in woods
@@ -18,35 +21,45 @@
 
 
 rules = [
-"RM,I,Iron Ore,IR,HIO,0.5",
-"RM,I,Copper Ore,CR,HCO,0.5",
-"RM,I,Stone,ST,HST,0.5",
-"RM,I,Wood,WD,HWO,0.5",
-"RM,I,Coal,CB,HCB,0.5",
-"RM,F,Crude Oil,HPO,HWO,0.5",
+"RM,Iron Ore,IR,HIO,0.5",
+"RM,Copper Ore,CR,HCO,0.5",
+"RM,Stone,ST,HST,0.5",
+"RM,Wood,WD,HWO,0.5",
+"RM,Coal,CB,HCB,0.5",
+"RF,Crude Oil,HPO,HWO,0.5",
 
-"IT,I,Iron Plates,IPL,F,3.2,IR:1",
-"IT,I,Copper Plates,CPL,F,3.2,CR:1",
+"IT,Iron Plates,IPL,F,3.2,IR:1",
+"IT,Copper Plates,CPL,F,3.2,CR:1",
 
-"IT,I,Gears,GER,CARBG,0.5,IPL:2",
-"IT,I,Pipes,PIP,CARBG,0.5,IPL:1",
+"IT,Gears,GER,CARBG,0.5,IPL:2",
+"IT,Pipes,PIP,CARBG,0.5,IPL:1",
 
-"IT,I,Coil,COI,CARBG,0.5,CPL:0.5",
-"IT,I,Yellow Belt,YBT,CARBG,0.5,IPL:0.5,GER:0.5",
-"IT,I,Green Circuits,GCI,CARBG,0.5,IPL:1,COI:1",
+"IT,Coil,COI,CARBG,0.5,CPL:0.5",
+"IT,Yellow Belt,YBT,CARBG,0.5,IPL:0.5,GER:0.5",
+"IT,Green Circuits,GCI,CARBG,0.5,IPL:1,COI:1",
 
-"IT,I,Offshore Pump,OFP,CARBG,0.5,GER:1,PIP:1,GCI:2",
-"IT,I,Stone Furnace,STF,CARBG,0.5,ST:5",
-"IT,I,Boiler,BOL,CARBG,0.5,PIP:4,STF:1",
-"IT,I,Steam Engine,SEG,CARBG,0.5,IPL:10,GER:8,PIP:5",
-"IT,I,Small Power Pole,SPP,CARBG,0.5,WD:1,CPL:1",
+"IT,Offshore Pump,OFP,CARBG,0.5,GER:1,PIP:1,GCI:2",
+"IT,Stone Furnace,STF,CARBG,0.5,ST:5",
+"IT,Boiler,BOL,CARBG,0.5,PIP:4,STF:1",
+"IT,Steam Engine,SEG,CARBG,0.5,IPL:10,GER:8,PIP:5",
+"IT,Small Power Pole,SPP,CARBG,0.5,WD:1,CPL:1",
 
-"IT,I,Lab,LAB,CARBG,2,YBT:4,GER:10,GCI:10",
-"IT,I,Red Science,RSI,CARBG,5,CPL:1,GER:1",
-"IT,I,Automation Research,ATRS,LAB,10,RSI:10,OFP:1,BOL:1,SEG:1,SPP:1,LAB:1",
+"IT,Lab,LAB,CARBG,2,YBT:4,GER:10,GCI:10",
+"IT,Red Science,RSI,CARBG,5,CPL:1,GER:1",
+"IT,Automation Research,ATRS,LAB,10,RSI:10,OFP:1,BOL:1,SEG:1,SPP:1,LAB:1",
 
-"IT,I,Assembler 1 Red,AS1,CARBG,IPL:9,GER:5,GCI:3"
+"IT,Assembler 1 Red,AS1,CARBG,IPL:9,GER:5,GCI:3"
 ]
+
+rulesj = {
+"Iron Ore":{"rtyp":"RawMat","time":"0.5","Abr":"IRO","harvest":"Iron Ore Patch","method":"Person,Miner"},
+"Copper Ore":{"rtyp":"RawMat","Time":"0.5","Abr":"CPO","harvest":"Copper Ore Patch","method":"Person,Miner"},
+"Stone":{"rtyp":"RawMat","Time":"0.5","Abr":"CPO","harvest":"Stone Patch","method":"Person,Miner"},
+"Wood":{"rtyp":"RawMat","Time":"0.5","Abr":"WOD","harvest":"Woods","method":"Person"},
+"Coal":{"rtyp":"RawMat","Time":"0.5","Abr":"COL","harvest":"Coal Patch","method":"Person,Miner"},
+"Oil":{"rtyp":"RawFluid","Time":"0.5","Abr":"OIL","harvest":"Oil Patch","method":"Oil Pump"},
+}
+
 
 
 print(f"Lazy Bastard - there are {len(rules)} rules")
