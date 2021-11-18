6de19a9330b211d8f67e75728fa109533ec4effa
MikeWise2718 <mikewise1618@gmail.com>
2021-11-18 23:26:54 +0100

    added rules and rule checking

--- lazyb.py
+++ lazyb.py
@@ -20,10 +20,10 @@ starttime = time.time()
 # Type - RM Raw Material, IT Basic Item, F Fluid
 
 
-rules = [
+ruless = [
 "RM,Iron Ore,IR,HIO,0.5",
 "RM,Copper Ore,CR,HCO,0.5",
-"RM,Stone,ST,HST,0.5",
+"RM,Stone,STO,HST,0.5",
 "RM,Wood,WD,HWO,0.5",
 "RM,Coal,CB,HCB,0.5",
 "RF,Crude Oil,HPO,HWO,0.5",
@@ -51,15 +51,69 @@ rules = [
 "IT,Assembler 1 Red,AS1,CARBG,IPL:9,GER:5,GCI:3"
 ]
 
-rulesj = {
-"Iron Ore":{"rtyp":"RawMat","time":"0.5","Abr":"IRO","harvest":"Iron Ore Patch","method":"Person,Miner"},
-"Copper Ore":{"rtyp":"RawMat","Time":"0.5","Abr":"CPO","harvest":"Copper Ore Patch","method":"Person,Miner"},
-"Stone":{"rtyp":"RawMat","Time":"0.5","Abr":"CPO","harvest":"Stone Patch","method":"Person,Miner"},
-"Wood":{"rtyp":"RawMat","Time":"0.5","Abr":"WOD","harvest":"Woods","method":"Person"},
-"Coal":{"rtyp":"RawMat","Time":"0.5","Abr":"COL","harvest":"Coal Patch","method":"Person,Miner"},
-"Oil":{"rtyp":"RawFluid","Time":"0.5","Abr":"OIL","harvest":"Oil Patch","method":"Oil Pump"},
+rules = {
+"Iron Ore":{"rtyp":"RawMat","T":0.5,"Ab":"IRO","harvest":"Iron Ore Patch","meth":"Craft,Miner"},
+"Copper Ore":{"rtyp":"RawMat","T":0.5,"Ab":"CPO","harvest":"Copper Ore Patch","meth":"Craft,Miner"},
+"Stone":{"rtyp":"RawMat","T":0.5,"Ab":"STO","harvest":"Stone Patch","meth":"Craft,Miner"},
+"Wood":{"rtyp":"RawMat","T":0.5,"Ab":"WOD","harvest":"Woods","meth":"Craft"},
+"Coal":{"rtyp":"RawMat","T":0.5,"Ab":"COL","harvest":"Coal Patch","meth":"Craft,Miner"},
+"Oil":{"rtyp":"RawFluid","T":0.5,"Ab":"OIL","harvest":"Oil Patch","meth":"Oil Pump"},
+
+"Iron Plates":{"rtyp":"Item","T":3.2,"Ab":"IPL","meth":"Furnace","Req":{"IRO":1}},
+"Copper Plates":{"rtyp":"Item","T":3.2,"Ab":"CPL","meth":"Furnace","Req":{"CPO":1}},
+
+"Gears":{"rtyp":"Item","T":0.5,"Ab":"GER","meth":"Craft,ARBG","Req":{"IPL":2}},
+"Pipes":{"rtyp":"Item","T":0.5,"Ab":"PIP","meth":"Craft,ARBG","Req":{"IPL":1}},
+
+"Coil":{"rtyp":"Item","T":0.5,"Ab":"COI","meth":"Craft,ARBG","Req":{"CPL":0.5}},
+"Yellow Belt":{"rtyp":"Item","T":0.5,"Ab":"YBT","meth":"Craft,ARBG","Req":{"IPL":0.5,"GER":0.5}},
+"Green Circuits":{"rtyp":"Item","T":0.5,"Ab":"GCI","meth":"Craft,ARBG","Req":{"IPL":1,"COI":1}},
+
+"Offshore Pump":{"rtyp":"Item","T":0.5,"Ab":"OFP","meth":"Craft,ARBG","Req":{"GER":1,"PIP":1,"GCI":2}},
+"Stone Furnace":{"rtyp":"Item","T":0.5,"Ab":"STF","meth":"Craft,ARBG","Req":{"STO":5}},
+"Boiler":{"rtyp":"Item","T":0.5,"Ab":"BOL","meth":"Craft,ARBG","Req":{"PIP":4,"STF":1}},
+"Steam Engine":{"rtyp":"Item","T":0.5,"Ab":"SEG","meth":"Craft,ARBG","Req":{"IPL":10,"GER":8,"PIP":5}},
+"Small PowerPole":{"rtyp":"Item","T":0.5,"Ab":"SPP","meth":"Craft,ARBG","Req":{"WOD":1,"CPL":1}},
+
+"Lab":{"rtyp":"Item","T":2,"Ab":"LAB","meth":"Craft,ARBG","Req":{"YBT":4,"GER":10,"GCI":10}},
+"Red Science":{"rtyp":"Item","T":5,"Ab":"RSI","meth":"Craft,ARBG","Req":{"CPL":1,"GER":1}},
+"Automation Research":{"rtyp":"Item","T":5,"Ab":"ATR","meth":"Lab","Req":{"RSI":1,"OFP":1,"BOL":1,"SEG":1,"SPP":1,"LAB":1}},
+
+"Assembler 1":{"rtyp":"Item","T":5,"Ab":"AS1","meth":"Craft,ARBG","Req":{"IPL":9,"GER":5,"GCI":3}},
 }
+
+class RulingClass:
+
+    abdict = {}
+
+    def __init__(self):
+        i = 0
+        self.abdict = {}
+        for itemname,rule in rules.items():
+            ab = rule["Ab"]
+            self.abdict[ab] = rule
+            print(f"{i} {itemname} {ab} len:{len(self.abdict)}")
+            i += 1
+
+        # check requirements correctness
+        nbad = 0
+        nok = 0
+        for itemname,rule in rules.items():
+            rtyp = rule["rtyp"]
+            if rtyp=="Item":
+                req = rule["Req"]
+                for reqab,reqquant in req.items():
+                    if not reqab in self.abdict:
+                        print(f"Error - bad abreviation {reqab} in rule {itemname}")
+                        nbad += 1
+                    else:
+                        nok += 1
+        print(f"Checked rule abreviations in reqs nok:{nok} nbad:{nbad}")
 
 
 
 print(f"Lazy Bastard - there are {len(rules)} rules")
+
+rc = RulingClass()
+
+
